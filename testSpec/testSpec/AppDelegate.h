01557b10ce98330a96fa9e820dd975cbe46f90cd
ZL <zl@ZLdeMacBook-Pro.local>
2016-03-02 16:39:24 +0800

    first commit

---
+++ testSpec/testSpec/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testSpec
+//
+//  Created by ZL on 16/3/2.
+//  Copyright © 2016年 ZL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
